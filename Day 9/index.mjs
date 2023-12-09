67f78ee3e30a210aed8009722202720e53a0a9e7
Pelagoss <pelagoss@outlook.fr>
2023-12-09 17:56:27 +0100

    Day 9 done

--- Day 9/index.mjs
+++ Day 9/index.mjs
@@ -1,9 +1,50 @@
 function solvePartOne(data) {
-    return undefined;
+    return data.map((element, line) => {
+        let numbers = element.split(' ').map((n) => parseInt(n));
+
+        let copyNumbers = JSON.parse(JSON.stringify(numbers));
+        let steps = [copyNumbers]
+        
+        
+        while (copyNumbers.some((n) => n !== 0)) {
+            let i = 0;
+            let differences = [];
+
+            while(i < copyNumbers.length-1) {
+                differences.push(copyNumbers[i+1] - copyNumbers[i])
+                i++;
+            }
+
+            steps.push(differences)
+            copyNumbers = JSON.parse(JSON.stringify(differences));
+        }
+
+        steps.reverse();
+
+        steps[0].push(0)
+        
+        let j = 0;
+        let last_number = 0;
+        while(j < steps.length-1) {
+            last_number = steps[j+1][steps[j+1].length - 1] + last_number;
+            steps[j+1].push(last_number)
+            j++;
+        }
+        
+        return last_number
+    }).reduce((a, b) => a + b, 0);
 }
 
 function solvePartTwo(data) {
-    return undefined;
+    data = data.map((element, line) => {
+        let numbers = element.split(' ').map((n) => parseInt(n));
+
+        numbers.reverse()
+
+        return numbers.join(' ');
+    })
+
+    return solvePartOne(data);
 }
 
 export const solve = (data) => {
