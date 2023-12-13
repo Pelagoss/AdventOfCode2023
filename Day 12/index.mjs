afefb66b883717d6113a214c87cad9792aa2e2d8
Cyril HERRERA <pelagoss@outlook.fr>
2023-12-13 15:47:52 +0100

    day 12 done

--- Day 12/index.mjs
+++ Day 12/index.mjs
@@ -1,70 +1,5 @@
 import { Worker } from "worker_threads";
 
-function get_possible_count(springs, groups, prev_size = 0, must_operational = false) {
-    if (springs == "") {
-        if (groups.length > 0) {
-            if (groups.length == 1 && groups[0] == prev_size) {
-                return 1;
-            }
-
-            return 0;
-        } else {
-            if (prev_size == 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    }
-
-    if (groups.length == 0) {
-        if (springs.includes("#") || prev_size > 0) {
-            return 0;
-        }
-        
-        return 1;
-    }
-    
-    let curr = springs[0];
-    let rest = springs.slice(1);
-
-    if (curr == "?") {
-        return get_possible_count("#" + rest, groups, prev_size, must_operational) + get_possible_count("." + rest, groups, prev_size, must_operational);
-    }
-
-    if (curr == "#") {
-        if (must_operational) {
-            return 0;
-        }
-
-        let curr_size = prev_size + 1;
-
-        if (curr_size > groups[0]) {
-            return 0;
-        } else if (curr_size == groups[0]) {
-            return get_possible_count(rest, groups.slice(1), 0, true);
-        } else {
-            return get_possible_count(rest, groups, curr_size, false);
-        }
-    }
-    
-    if (curr == ".") {
-        if (must_operational === true) {
-            return get_possible_count(rest, groups, 0, false);
-        }
-        
-        if (prev_size == 0) {
-            return get_possible_count(rest, groups, 0, false);
-        } else {
-            if (prev_size != groups[0]) {
-                return 0;
-            } else {
-                return get_possible_count(rest, groups.slice(1), 0, false);
-            }
-        }
-    }
-}
-
 function createWorker(data) {
     return new Promise(function (resolve, reject) {
         let worker = new Worker("./Day 12/worker.mjs", {
@@ -90,10 +25,10 @@ async function solvePartOne (data) {
         let springs = parts[0];
         let groups = parts[1].split(',').map((g) => parseInt(g));
 
-        return {springs, groups};
+        return {springs, groups}
     })
 
-    let chunkSize  = Math.floor(data.length/6);
+    let chunkSize  = Math.floor(data.length/4);
 
     for (let i = 0; i < data.length; i += chunkSize) {
         workerPromises.push(createWorker(data.slice(i, i + chunkSize)));
--- Day 12/worker.mjs
+++ Day 12/worker.mjs
@@ -1,77 +1,60 @@
 import { workerData, parentPort } from "worker_threads";
 
-function get_possible_count(springs, groups, prev_size = 0, must_operational = false, cache) {
-    cache = cache || {};
-    
-    let cacheKey = JSON.parse(JSON.stringify({springs, groups, prev_size, must_operational}));
+function memoize(func) {
+    const stored = new Map();
 
-    if (cache[cacheKey]) {
-        return cache[cacheKey];
-    }
-
-    if (springs == "") {
-        if (groups.length > 0) {
-            if (groups.length == 1 && groups[0] == prev_size) {
-                return 1;
-            }
-
-            return 0;
-        } else {
-            if (prev_size == 0) {
-                return 1;
-            } else {
-                return 0;
-            }
+    return (...args) => {
+        const k = JSON.stringify(args);
+        if (stored.has(k)) {
+        return stored.get(k);
         }
-    }
-
-    if (groups.length == 0) {
-        if (springs.includes("#") || prev_size > 0) {
-            return 0;
-        }
-        
-        return 1;
-    }
-    
-    let curr = springs[0];
-    let rest = springs.slice(1);
-
-    if (curr == "?") {
-        return get_possible_count("#" + rest, groups, prev_size, must_operational) + get_possible_count("." + rest, groups, prev_size, must_operational);
-    }
-
-    if (curr == "#") {
-        if (must_operational) {
-            return 0;
-        }
-
-        let curr_size = prev_size + 1;
-
-        if (curr_size > groups[0]) {
-            return 0;
-        } else if (curr_size == groups[0]) {
-            return get_possible_count(rest, groups.slice(1), 0, true);
-        } else {
-            return get_possible_count(rest, groups, curr_size, false);
-        }
-    }
-    
-    if (curr == ".") {
-        if (must_operational === true) {
-            return get_possible_count(rest, groups, 0, false);
-        }
-        
-        if (prev_size == 0) {
-            return get_possible_count(rest, groups, 0, false);
-        } else {
-            if (prev_size != groups[0]) {
-                return 0;
-            } else {
-                return get_possible_count(rest, groups.slice(1), 0, false);
-            }
-        }
-    }
+        const result = func(...args);
+        stored.set(k, result);
+        return result;
+    };
 }
 
+const countWays = memoize((line, runs) => {
+    if (line.length === 0) {
+        if (runs.length === 0) {
+        return 1;
+        }
+        return 0;
+    }
+    if (runs.length === 0) {
+        for (let i = 0; i < line.length; i++) {
+        if (line[i] === "#") {
+            return 0;
+        }
+        }
+        return 1;
+    }
 
-parentPort.postMessage(workerData.data.map((d) => get_possible_count(d.springs, d.groups)).reduce((a, b) => a + b, 0));
+    if (line.length < runs.reduce((a, b) => a + b, 0) + runs.length - 1) {
+        return 0;
+    }
+
+    if (line[0] === ".") {
+        return countWays(line.slice(1), runs);
+    }
+    if (line[0] === "#") {
+        const [run, ...leftoverRuns] = runs;
+        for (let i = 0; i < run; i++) {
+        if (line[i] === ".") {
+            return 0;
+        }
+        }
+        if (line[run] === "#") {
+        return 0;
+        }
+
+        return countWays(line.slice(run + 1), leftoverRuns);
+    }
+
+    return (
+        countWays("#" + line.slice(1), runs) + countWays("." + line.slice(1), runs)
+    );
+});
+
+
+parentPort.postMessage(workerData.data.map((d) => countWays(d.springs, d.groups)).reduce((a, b) => a + b, 0));
